f9d3e7bf8a643e7526af4b93560853b53f8d5dc6
Harshit9369 <harshitagrwl911@gmail.com>
2024-08-27 19:54:53 +0530

    deployment

---
+++ config.py
@@ -0,0 +1,13 @@
+DB_CONFIG = {
+    'dbname': 'influencer_db',
+    'user': 'harry',
+    'password': 'harsh9369',
+    'host': 'localhost',
+    'port': '5432'
+}
+
+API_CONFIG = {
+    'url': 'https://youtube-influencer-search.p.rapidapi.com/searches/5/results',
+    'key': 'cd1a5edfabmsh5af28e0992280c8p1ba7d5jsn0e765a4d2172',
+    'host': 'youtube-influencer-search.p.rapidapi.com'
+}
